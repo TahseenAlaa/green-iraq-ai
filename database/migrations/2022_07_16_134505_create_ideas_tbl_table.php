4c914f6e00863acf351cb058dc36a418ef51f03a
Tahseen Alaa <raspoteen7@gmail.com>
2022-07-16 16:57:02 +0300

    Create ideas controller, model, and migration

---
+++ database/migrations/2022_07_16_134505_create_ideas_tbl_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ideas_tbl', function (Blueprint $table) {
+            $table->id();
+            $table->longText('idea')->unique();
+            $table->integer('upvote')->nullable();
+            $table->integer('downvote')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ideas_tbl');
+    }
+};
